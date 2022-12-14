93624acb4a369fb7b7c59b41d17fb1c62faff205
Victor Matias <fvictor1705@gmail.com>
2022-12-14 18:07:09 -0300

    fix: connection.ts

--- src/models/connection.ts
+++ src/models/connection.ts
@@ -2,7 +2,7 @@ import mysql from 'mysql2/promise';
 
 const connection = mysql.createPool({
   host: process.env.MYSQL_HOST,
-  user: process.env.MYQSL_USER,
+  user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
 });
 
